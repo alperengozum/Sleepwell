d6712504bd2a53ef474832ffa333d4b0018d5891
alperengozum <alperengozum0@gmail.com>
2025-12-24 22:29:31 +0300

    feat: Add alarm translations for multiple languages and update AlarmUtils to utilize i18n for alarm messages

--- src/utils/AlarmUtils.ts
+++ src/utils/AlarmUtils.ts
@@ -1,23 +1,33 @@
 import {useSleepStore, SleepType} from "../store/SleepStore";
 import {setAlarm} from "expo-alarm";
 import Toast from "react-native-toast-message";
+import i18n from "../i18n";
 
 export const createIntentAlarm = (date: Date, type?: SleepType, cycleCount?: number): void => {
   let createDate: Date = new Date();
+  const alarmType = type || SleepType.SLEEP;
+  const alarmMessage = alarmType === SleepType.SLEEP 
+    ? i18n.t('alarm.sleep') 
+    : i18n.t('alarm.powernap');
+  
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const timeString = `${hours}:${("0" + minutes).slice(-2)}`;
+  
   setAlarm({
-    hour: date.getHours(),
-    minutes: date.getMinutes(),
-    message: type || SleepType.SLEEP
+    hour: hours,
+    minutes: minutes,
+    message: alarmMessage
   }).then(() => {
     Toast.show({
       type: "success",
       position: "bottom",
-      text1: "Alarm set",
-      text2: `Alarm set for ${date.getHours()}:${date.getMinutes()} Sleepwell!`,
+      text1: i18n.t('alarm.set'),
+      text2: i18n.t('alarm.setFor', { time: timeString }),
       autoHide: true,
     })
   })
   useSleepStore.getState().addSleep({
-    end: date, start: createDate, type: type || SleepType.SLEEP, cycle: cycleCount || undefined
+    end: date, start: createDate, type: alarmType, cycle: cycleCount || undefined
   })
 }
